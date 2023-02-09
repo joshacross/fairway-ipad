551051dacf3b4fe9af3edd32661bf732678940bd
Joshua Cross <74011006+joshacross@users.noreply.github.com>
2023-02-09 01:30:08 -0600

    added router and created an editable page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/theme";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
